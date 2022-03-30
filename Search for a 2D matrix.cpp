c937e29fe266cf975b314bd3f53a31de863d47e4
Bittu Pandey <bittu.pandey56@yahoo.com>
2022-03-30 23:36:30 +0530

    search for a 2 D matrix

---
+++ Search for a 2D matrix.cpp
@@ -0,0 +1,25 @@
+/*Search a 2D Matrix*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) { 
+        if(!matrix.size()) return false;
+        int n= matrix.size();
+        int m=matrix[0].size();
+        
+      
+        int left=0;
+        int right=n*m-1;
+        
+        while(left<=right)
+        {
+            int mid=(left+(right-left)/2);
+            if(matrix[mid/m][mid%m]==target) return true;
+            if( matrix[mid/m][mid%m]<target)left=mid+1;
+            else
+                right= mid-1;
+         }
+        
+        return false;
+    }
+};
