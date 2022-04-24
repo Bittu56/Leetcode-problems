f9f20fef9d56a8589d325ed43a265bb2948b7eea
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-04-24 10:27:42 +0530

    Time: 4 ms (69.85%), Space: 7.7 MB (5.84%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    unordered_map<string, string> codeDB, urlDB;
-    const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+unordered_map <string,string>codedb,urldb;
+   const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    // Encodes a URL to a shortened URL.
     
-    string getCode() {
-        string code = "";
-        for (int i = 0; i < 6; i++) code += chars[rand() % 62];
+    string getcode()
+    {
+        string code="";
+        for(int i=0;i<6;i++)
+        {
+            code+=chars[rand()%62];
+        }
+        
         return "http://tinyurl.com/" + code;
     }
-    
     string encode(string longUrl) {
-        if (urlDB.find(longUrl) != urlDB.end()) return urlDB[longUrl];
-        string code = getCode();
-        while (codeDB.find(code) != codeDB.end()) code = getCode();
-        codeDB[code] = longUrl;
-        urlDB[longUrl] = code;
+        
+    if(urldb.find(longUrl)!=urldb.end()) return urldb[longUrl];
+        string code=getcode();
+        while (codedb.find(code) != codedb.end()) code = getcode();
+        
+        codedb[code] = longUrl;
+        urldb[longUrl]=code;
+        
         return code;
-    }
+ }
 
+    // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return codeDB[shortUrl];
+        return codedb[shortUrl];
+        
     }
 };
 
