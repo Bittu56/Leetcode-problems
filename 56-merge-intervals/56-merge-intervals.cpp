04848438f9d189c56349ceab6ef48bd7042fd774
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-04-30 16:02:04 +0530

    Time: 41 ms (75.71%), Space: 20.1 MB (27.44%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-         vector<vector<int>> mergedIntervals;
-        if(intervals.size() == 0) {
-            return mergedIntervals; 
-        }
-        sort(intervals.begin(), intervals.end());
-        vector<int> tempInterval = intervals[0]; 
+       
         
-        for(auto it : intervals) {
-            if(it[0] <= tempInterval[1]) {
-                tempInterval[1] = max(it[1], tempInterval[1]); 
-            } else {
-                mergedIntervals.push_back(tempInterval); 
-                tempInterval = it; 
+        vector<vector<int>>merged;
+        
+        if(intervals.size()==0)
+            return merged;
+        sort(intervals.begin(),intervals.end());
+        vector<int> temp= intervals[0];
+        
+        for(auto itr:intervals)
+        {
+            if(itr[0]<=temp[1])
+            {
+                temp[1]=max(itr[1],temp[1]);
+                temp[0]=min(itr[1],temp[0]);
+            }
+            else
+            {
+                merged.push_back(temp);
+                temp=itr;
             }
         }
-        mergedIntervals.push_back(tempInterval);
-        return mergedIntervals; 
+        
+        merged.push_back(temp);
+        return merged;
+        
+        
     }
 };
