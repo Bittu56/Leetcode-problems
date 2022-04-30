7438382d8ac8c905b3afc37d4c0c7e417186f159
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-04-30 23:12:46 +0530

    Time: 5 ms (30.21%), Space: 9.1 MB (31.47%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,6 +3,7 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
        
         int i=m-1,j=n-1,k=m+n-1;
+        sort(nums2.begin(),nums2.end());
         while(i>=0 && j>=0)
         {
             if(nums1[i]>nums2[j])
