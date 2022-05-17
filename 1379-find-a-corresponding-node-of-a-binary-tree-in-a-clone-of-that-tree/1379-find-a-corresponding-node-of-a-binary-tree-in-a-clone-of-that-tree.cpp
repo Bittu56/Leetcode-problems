5a439492584c67dd0151baa25d4951f866b5942a
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-05-17 21:05:53 +0530

    Time: 836 ms (26.16%), Space: 164 MB (22.94%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        if (original == nullptr) return nullptr;
+        if (original == nullptr) return original;
 		//Don't compare value. Compare the pointer
         if (original == target) return cloned;
         TreeNode* left = getTargetCopy(original->left, cloned->left, target);
