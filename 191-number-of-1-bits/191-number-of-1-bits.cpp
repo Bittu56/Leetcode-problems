39abc452561afd70c86e21250c9f53f94fbad596
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-05-27 16:12:43 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.75%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,8 +4,11 @@ public:
         int ans = 0;
         while(n > 0)
         {
-            if(n%2==1)ans++;
-            n /= 2;
+            if(n&1)
+            {
+                ans++;
+            }
+            n=n>>1;
         }
         return ans;
     }
