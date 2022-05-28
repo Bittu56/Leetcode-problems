0fa1d851ed95fc3478947cea287fdf8f99b7269c
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-05-28 10:00:10 +0530

    Time: 3 ms (33.94%), Space: 6 MB (26.52%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
-int numberOfSteps (int num) {
-  return num == 0 ? 0 : log2(num) + bitset<32>(num).count();
-}
+    int numberOfSteps (int num) {
+        int count=0;
+        while(num){
+            if(num%2){ // odd
+                --num;
+            }
+            else{ // even
+                num>>=1; // num=num/2
+            }
+            ++count;
+        }
+        return count;
+    }
 };
