9baf7e1eeaf36f70f6a5c212b020d5fbe2f6f112
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-06-02 13:10:42 +0530

    Time: 15 ms (65.95%), Space: 10.7 MB (43.03%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -4,10 +4,10 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         
-        vector<vector<int>> res(m,vector<int> (n,0)); // matrix creation
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                res[j][i] = matrix[i][j]; // filling the matrix in transpose form
+        vector<vector<int>> res(m,vector<int> (n,0)); 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                res[i][j] = matrix[j][i]; 
             }
         }
         return res;
