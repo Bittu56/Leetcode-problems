c8c929d674a44774cd6c2419c5bee06d6690a005
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-06-06 16:19:11 +0530

    Time: 16 ms (23.07%), Space: 14.9 MB (43.59%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -17,7 +17,7 @@ public:
         if(list2==NULL)
           return list1;
         
-        if(list1->val<list2->val)
+        if(list1->val<=list2->val)
         {
             list1->next=mergeTwoLists(list1->next,list2);
             return list1;
