e3b554530ff5f224225a2ffa506b556984cd6a6e
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-06-12 12:12:21 +0530

    Time: 744 ms (16.77%), Space: 127.3 MB (24.42%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -6,18 +6,26 @@ public:
         int ans=0;
         int l=0,r=0;
         
-        unordered_set<int>s;
+      unordered_set<int>s;
         
         while(r<nums.size())
         {
-            while(s.find(nums[r]) != end(s))
+            while(s.find(nums[r])!=s.end())
             {
-              curr_sum-=nums[l];
-              s.erase(nums[l++]);
+                curr_sum-=nums[l];
+                s.erase(nums[l]);
+                l++;
+              
             }
-              curr_sum += nums[r], s.insert(nums[r++]);  
-		ans = max(ans, curr_sum);                
-	}
-	return ans;
-}
+            curr_sum+=nums[r];
+            s.insert(nums[r]);
+            r++;
+          
+            ans = max(ans, curr_sum);
+        }
+        
+        return ans;
+    }
 };
+        
+        
