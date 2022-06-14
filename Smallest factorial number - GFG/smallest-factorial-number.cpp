24c165f79d9612f059ae73117b59e40b1d7a4430
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-06-14 09:48:07 +0530

    Added solution - LeetHub

---
+++ Smallest factorial number - GFG/smallest-factorial-number.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+        int get_count(int n)
+    {
+        int cnt=0;
+        while(n>=5)
+        {
+            cnt=cnt+(n/5);
+            n=n/5;
+        }
+        return cnt;
+    }
+    int findNum(int n)
+    {
+       
+        int low=0,high=1e9,ans=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int cnt=get_count(mid);
+            if(cnt>=n)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        Solution ob;
+        cout <<ob.findNum(n) << endl;
+    }
+return 0;
+}  // } Driver Code Ends
