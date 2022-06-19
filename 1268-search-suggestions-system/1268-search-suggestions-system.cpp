6ed45512dae0248f7dc7363fd7dc466670905d6c
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-06-19 23:20:39 +0530

    Time: 1455 ms (6.78%), Space: 331.3 MB (11.66%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(), products.end());
-        vector<vector<string>> ans;
-        int left = 0, right = products.size() - 1;
-        for (int i = 0; i < searchWord.length(); i++) {
-            vector<string> res;
-            char c = searchWord[i];
-            while (left <= right && (products[left].length() == i || products[left][i] < c)) left++;
-            while (left <= right && (products[right].length() == i || products[right][i] > c)) right--;
-            for (int j = 0; j < 3 && left + j <= right; j++)
-                res.push_back(products[left+j]);
-            ans.push_back(res);
+vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        int n=products.size();
+        sort(products.begin(),products.end());
+        vector<vector<string>>ans;
+        vector<string>temp;
+        string curr="";
+        for(auto c:searchWord){
+            curr+=c;                // adding character, like -> m-->mo-->mou-->mous--->mouse
+            temp.clear();         //reusing the same vector
+            for(int i=0;i<n;i++){
+                string s=products[i];
+                if(s.substr(0,curr.length())==curr){              //finding the prefix containing words in the list
+                    temp.push_back(s);
+                }
+                if(temp.size()==3)break;         //question asked for 3 words so we break at 3
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
 };
-        
-   
