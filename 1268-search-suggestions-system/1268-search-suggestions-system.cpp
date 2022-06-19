cd4af1f62348d887897c7bac7cafe137438e6ab5
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-06-19 23:43:06 +0530

    Time: 1687 ms (5.04%), Space: 331.3 MB (11.66%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
 vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        int n=products.size();
-        sort(products.begin(),products.end());
-        vector<vector<string>>ans;
-        vector<string>temp;
-        string curr="";
-        for(auto c:searchWord){
-            curr+=c;               
-            temp.clear();         
-            for(int i=0;i<n;i++){
-                string s=products[i];
-                if(s.substr(0,curr.length())==curr){              
+       int n=products.size();
+    vector<vector<string>> ans;
+    sort(products.begin(),products.end());
+    vector<string>temp;
+    string curr="";
+    
+    for(auto c:searchWord)
+    {
+        curr+=c;
+        temp.clear();
+        for(int i=0;i<n;i++)
+        { string s=products[i];
+              if(s.substr(0,curr.length())==curr){              
                     temp.push_back(s);
                 }
-                if(temp.size()==3)break;         
-            }
-            ans.push_back(temp);
+            if(temp.size()==3) break;
+         }
+ ans.push_back(temp);
         }
         return ans;
     }
-};
+               };
