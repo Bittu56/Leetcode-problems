2335da070ce30054a29c451026ee3f8500e44891
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-06-22 00:53:52 +0530

    Time: 199 ms (22.23%), Space: 53.9 MB (34.72%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-    
-    
-    bool check(vector<int> heights, int bricks, int ladders, int mid){
-     
-		vector<int> diff;
-        for(int i = 1; i <= mid; i++){
-            if(heights[i] > heights[i-1]){
-                diff.push_back(heights[i]-heights[i-1]);
-            }
-        }
-		
-        sort(diff.begin(), diff.end(), greater<int>());
-        int l = diff.size();
-        for(int i = ladders; i < l; i++){
-            if(diff[i] > bricks) return false;
-            bricks -= diff[i];
-        }
-        return true;
-    }
-    
-    
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n = heights.size();
-        int lo = 0, hi = n-1;
-        while(lo < hi){
-            int mid = (lo+hi+1)/2;
-            if(check(heights, bricks, ladders, mid)){
-                lo = mid;  
-            }
-            else{
-                hi = mid-1; 
-            }
+        
+            int i;
+            
+            priority_queue<int,vector<int>> pq;
+            
+            int difference;
+            
+            for (int i=0;i<heights.size()-1;i++)
+            {
+                difference =heights[i+1]-heights[i];
+                if(difference>0)
+                {
+                    bricks-=difference;
+                    pq.push(difference);
+                   if(bricks<0)
+                   {
+                       
+                       if(ladders <=0) return i;
+                       bricks+=pq.top();
+                       pq.pop();
+                       ladders--;
+                   }
+                
+                }
+                
+                }
+            
+            
+            return i;
+            
         }
-        return lo;
-    }
-};
+    };
+  
