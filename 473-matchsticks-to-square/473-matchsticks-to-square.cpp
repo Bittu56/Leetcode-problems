bf53b8c5c31024fa368912fb7b674674da2c9f0c
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-07-14 13:58:10 +0530

    Time: 274 ms (59.87%), Space: 55 MB (8.52%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,34 +1,60 @@
 class Solution {
 public:
-    bool makesquare(vector<int>& M) {
-        sort(M.begin(), M.end(), greater<int>());
-        int total = accumulate(M.begin(), M.end(), 0);
-        side = total / 4;
-        if ((float)total / 4 > side || M[0] > side)
-            return false;
-        return btrack(0, side, 0, M);
-    }
-private:
-    int side;
-    bool btrack(int i, int space, int done, vector<int>& M) {
-        if (done == 3)
-            return true;
-        for (; i < M.size(); i++) {
-            int num = M[i];
-            bool res;
-            if (num > space)
-                continue;
-            M[i] = side + 1;
-            if (num == space)
-                res = btrack(1, side, done+1, M);
-            else
-                res = btrack(i+1, space-num, done, M);
-            if (res)
+    // DP OPTIMISATION, check whether same state has already happened or not?
+    bool already_happened(vector<int> sides,int pos){
+        for(int i=0;i<pos;i++){
+            if(sides[i]==sides[pos])
                 return true;
-            M[i] = num;
-            while (i < M.size() and M[i+1] == num)
-                i++;
         }
         return false;
+    }
+    // recursion
+    bool recurse(vector<int> sides,int pos,int target,vector<int> v){
+        // check if we reaches the target?
+        if(pos==v.size())
+            return sides[0]==target and sides[1]==target and sides[2]==target and sides[3]==target;
+ 
+        for(int i=0;i<4;i++){
+ 
+            // check whether on adding current matchstick length, increases the target?
+            // also, check whether we have already checked this state or not?
+            if(sides[i]+v[pos]>target or already_happened(sides,i))
+                continue;
+ 
+            // include current matchstick length
+            sides[i]+=v[pos];
+            if(recurse(sides,pos+1,target,v))
+                return true;
+ 
+            // reduce side length for further recursion
+            sides[i]-=v[pos];
+        }
+        return false;
+    }
+    bool makesquare(vector<int>& sticks) {
+        // sum of all matchsticks length
+        int sum = accumulate(sticks.begin(),sticks.end(),0);
+ 
+        // sorting in non-increasing order decreases the runtime as well as reduces the chance of TLE
+        // since, bigger sticks are going to be processed at first
+        sort(sticks.rbegin(),sticks.rend());
+ 
+        // sides vector
+        vector<int> sides(4,0);
+ 
+        // if total sum of lengths of matchsticks is not a multiple of 4, we can't make a square 
+        // also, there must be atleast 4 matchsticks
+        if(sum%4 or sticks.size()<4)
+            return false;
+ 
+        // sum is now our target length = length of each side
+        sum/=4;
+ 
+        // also, check if any of the side has length more than the target, we can't make the matchstick
+        if(sticks[0]>sum)
+            return false;
+ 
+        // recurse to find out if there exists any optimal square?
+        return recurse(sides,0,sum,sticks);
     }
 };
