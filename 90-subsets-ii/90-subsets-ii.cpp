a993c270ddc42000637e8f2996df4dde093e40e9
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-07-15 06:33:07 +0530

    Time: 16 ms (15.72%), Space: 15.9 MB (22.15%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,21 +1,39 @@
+
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;       //to store all susbets
-        vector<int> curr;         //to store current individual subset (that we will build)
-        sort(nums.begin(),nums.end());      //sort the array so that duplicates are adjacent 
-        helper(nums,ans,curr,0);       //we start from index 0
-        return ans;
-        
+    
+    void helper(int i, vector<int>nums, vector<int>&temp, vector<vector<int>>&ans,bool pre)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(temp);
+        }
+        else
+        {
+                helper(i+1, nums,temp,ans,false);
+            if(i>0&&nums[i]==nums[i-1]&&(!pre))
+                return;
+            
+        temp.push_back(nums[i]);
+        helper(i+1, nums,temp,ans,true); 
+     temp.pop_back();
+
+
+         }
     }
     
-    void helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, int idx){
-        ans.push_back(curr);       //we include current susbet into final ans
-        for(int i=idx;i<nums.size();i++){     //check for all possibilites
-            if(i>idx &&  nums[i]==nums[i-1]) continue;      //if duplicate then we continue
-            curr.push_back(nums[i]);     //we include nums[i] in current subset
-            helper(nums,ans,curr,i+1); 
-            curr.pop_back();         //to get subset without nums[i]
-        }
-    }  
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> temp;
+        
+        vector<vector<int>> ans;
+        
+        helper(0,nums,temp,ans,false);
+        
+        
+        return ans;
+        
+        
+    }
 };
