734b9bffd676bfd1a720bbfcf5c1ac316e9cadf6
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-07-19 16:42:53 +0530

    Time: 932 ms (74.91%), Space: 95.5 MB (80.78%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& M, int T) {
-        int xlen = M[0].size(), ylen = M.size(), ans = 0;
-        unordered_map<int, int> res;
-        for (int i = 0; i < ylen; i++)
-            for (int j = 1; j < xlen; j++)
-                M[i][j] += M[i][j-1];
-        for (int j = 0; j < xlen; j++)
-            for (int k = j; k < xlen; k++) {
-                res.clear();
-                res[0] = 1;
-                int csum = 0;
-                for (int i = 0; i < ylen; i++) {
-                    csum += M[i][k] - (j ? M[i][j-1] : 0);
-                    ans += res.find(csum - T) != res.end() ? res[csum - T] : 0;
-                    res[csum]++;
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size() , cols = matrix[0].size();
+        
+        if(rows < 1){
+            return 0;
+        }
+        
+        // calculate prefix sum for rows
+        for(int row = 0 ; row < rows ; row++){
+            for(int col = 1 ; col < cols ;col++){
+                matrix[row][col] =  matrix[row][col] + matrix[row][col -1];
+            }
+        }
+      
+        int count = 0 , sum ;
+        unordered_map<int , int> counter;
+        for(int colstart = 0 ; colstart < cols ;colstart++){
+            for(int col = colstart ; col < cols; col++){
+                counter.clear();
+                counter[0] = 1;
+                sum =0;
+                for(int row = 0 ; row < rows ;row++){
+                    sum += matrix[row][col] - (colstart > 0 ? matrix[row][colstart - 1] : 0 );
+                    count += (counter.find(sum- target) != counter.end() ? counter[sum-target] : 0);
+                    counter[sum]++;
                 }
             }
-        return ans;
+        }
+        return count;
     }
 };
