97dcd4a7f1ff2d512cb22e36690f28e184e75adf
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-07-23 23:12:49 +0530

    Time: 946 ms (42.68%), Space: 209.4 MB (40.01%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,72 +1,53 @@
 class Solution {
 public:
-    int arr[100000]; //used to store the result using indices
-    vector<int> countSmaller(vector<int>& nums) {
-        //we store the vector of pairs
-        
-        //why we use pair , since we need to know the index of the element in the original array 
-        vector<pair<int,int>> pairs;
-        for(int i=0;i<nums.size();i++)
-        {
-            pairs.push_back({nums[i],i});
-        }
-        
-        mergeSort(pairs,0,nums.size()-1);
-        vector<int> res(nums.size());
-        for(int i=0;i<nums.size();i++)
-        {
-            res[i]=arr[i];
-        }
-        return res;
-    }
-    void mergeSort(vector<pair<int,int>>& pairs,int low,int high)
+    void merge(vector<pair<int,int>>&arr,vector<int>&count,int l,int mid,int r)
     {
-        int mid=(low+high)/2;
-        if(low<high)
-        {
-            //need to split them
-            mergeSort(pairs,low,mid);
-            mergeSort(pairs,mid+1,high);
-            //merge them and calculate the result for every element
-            merge(pairs,low,mid+1,high);
+      vector<pair<int,int>>temp(r-l+1);
+      int i=l,j=mid+1,k=0;
+      while(i<=mid && j<=r)
+      {
+          if(arr[i].first<=arr[j].first)
+          {
+              temp[k++]=arr[j++];
+          }
+          else
+          {
+              count[arr[i].second]+=r-j+1;
+              temp[k++]=arr[i++];
+          }
+      }
+        while(i<=mid){
+            temp[k++] = arr[i++];
         }
+        while(j<=r){
+            temp[k++] = arr[j++];
+        }
+        
+        for(int i=l;i<=r;i++){
+            arr[i] = temp[i-l];
+        }
+        
     }
-    
-    void merge(vector<pair<int,int>>& pairs,int low,int mid,int high){
-        //mid is the starting element for the second array
-        int i=low;
-        int j=mid;
-        int count=0; //the number of elements smaller in the arr from mid to high
-        vector<pair<int,int>> temp(high-low+1);
-        int k=0;
-        while(i<=mid-1 && j<=high)
-        {
-            if(pairs[i].first<=pairs[j].first)
-            {
-                arr[pairs[i].second]+=count; //add the number of smaller elements
-                temp[k++]={pairs[i++]};
-            }
-            else{
-                //a smaller element found
-                count++;
-                temp[k++]={pairs[j++]};
-            }
-        }
-        //add the remaining first array element if left any
-        while(i<=mid-1)
-        {
-            arr[pairs[i].second]+=count; //add the number of smaller elements
-            temp[k++]={pairs[i++]}; 
-        }
-        //add the remaining second array elements if left any
-        while(j<=high)
-        {
-            temp[k++]={pairs[j++]};
-        }
-        //change the pairs to make it in sorted order 
-        for(int i=0;i<k;i++)
-        {
-            pairs[low++]=temp[i];
-        }
+    void mergeSort(vector<pair<int,int>>&arr,vector<int>&count,int l,int r)
+    {
+        if(l>=r)
+          return;
+        int mid=(l+r)/2;
+        mergeSort(arr,count,l,mid);
+        mergeSort(arr,count,mid+1,r);
+        merge(arr,count,l,mid,r);
+            
+        
+    }
+    vector<int> countSmaller(vector<int>& nums) {
+      int n=nums.size();
+      vector<pair<int,int>>arr(n);
+      for(int i=0;i<n;i++)
+      {
+          arr[i]={nums[i],i};
+      }
+      vector<int>count(n,0);
+      mergeSort(arr,count,0,n-1);
+      return count;
     }
 };
