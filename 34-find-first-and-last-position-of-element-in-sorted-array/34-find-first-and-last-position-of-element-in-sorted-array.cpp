83e3ab87bb3c8806c27ee4c0a4e9c8dc1c8a5b7a
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-07-25 10:22:19 +0530

    Time: 21 ms (10.97%), Space: 13.7 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int BinarySearch(vector<int> nums, int num, string find) {
-        int left = 0, right = nums.size() - 1, mid;
-        int result = -1;
-
-        while (left <= right) {
-            mid = (left + right) / 2;
-
-            if (nums[mid] == num) {
-                result = mid;
-                (find == "FIRST") ? right = mid - 1 : left = mid + 1;
-            }
-            else if (nums[mid] > num) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-
-        return result;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {BinarySearch(nums, target, "FIRST"), BinarySearch(nums, target, "LAST")};
+        int start=0,end=nums.size()-1,res=-1;
+        vector<int>ans;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                res=mid;
+                end=mid-1;
+            }
+            else if(target<nums[mid])
+                end=mid-1;
+            else
+                start=mid+1;   
+        }
+        ans.push_back(res);
+        start=0,end=nums.size()-1,res=-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                res=mid;
+                start=mid+1;
+            }
+            else if(target<nums[mid])
+                end=mid-1;
+            else
+                start=mid+1;   
+        }
+        ans.push_back(res);
+        return ans;
     }
-   
 };
