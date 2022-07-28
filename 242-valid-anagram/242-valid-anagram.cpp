5f936491798eb5cab3bac0859a2de23b4c15a6f7
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-07-28 23:25:32 +0530

    Time: 13 ms (61.26%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,10 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.size()!=t.size()) return false;
+       unordered_map<char,int> mp;
+        for(char c:s)
+            mp[c]++;
+        for(char c1:t)
+        mp[c1]--;
         
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        for(auto ref:mp)
+        {
+            if(ref.second)
+                return false;
+        }
         
-        return s==t;            
+        return true;
     }
 };
+            
