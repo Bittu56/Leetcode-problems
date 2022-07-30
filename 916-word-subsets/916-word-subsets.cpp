fff66cc1041378552935e70d66c4f6c19e769281
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-07-30 23:45:38 +0530

    Time: 350 ms (43.37%), Space: 102.3 MB (65.17%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,43 +1,51 @@
 class Solution {
 public:
-    vector<int> checkfreq(string &str)
+    
+    vector<int> checkfreq(string & str)   /// frequency find string and return only frequency
     {
         vector<int> freq(26);
-        for(auto &c:str)
+        
+        for(auto &ref:str)
         {
-            freq[c-'a']++;
+            freq[ref-'a']++;
+            
         }
         return freq;
-        
     }
     
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        
-     vector<int>maxfreq(26);
-      vector<string>ans; 
-        for(auto &ref:words2)
-        {
-            vector<int>freq=checkfreq(ref);
     
-        for(int i=0;i<26;i++)
-            maxfreq[i]=max(freq[i],maxfreq[i]);
+    vector<string>ans;
+    vector<int>maxfreq(26);
+        
+        for(auto &ref: words2)
+        {
+           vector<int>freq= checkfreq(ref);
+          for(int i=0;i<26;i++)
+         maxfreq[i]=max(freq[i],maxfreq[i]);
         }
         
-    
         for(auto &ref:words1)
         {
-        vector<int>freq=checkfreq(ref);
-        
-        int i=0;
-        for(;i<26;i++)
-        {
- if(freq[i]<maxfreq[i])
- break;
+            vector<int> freq= checkfreq(ref);
+        int i =0;
+            for(;i<26;i++)
+            {
+                if(freq[i]<maxfreq[i])
+                    break;
+            }
+            
+            if(i==26)ans.push_back(ref);
         }
-            if(i==26) ans.push_back(ref);
-        }
-        
         return ans;
-        
     }
 };
+        
+        
+        
+   
+        
+        
+        
+        
+    
