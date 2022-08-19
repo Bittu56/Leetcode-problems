a732bddca393bbbd425ca6c972290715530211a9
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-08-19 23:31:21 +0530

    Time: 235 ms (18.89%), Space: 59.1 MB (24.32%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& A) {
-        unordered_map<int, int> left, end;
-        for (int i: A) {
-            left[i]++;
+    bool isPossible(vector<int>& nums) {
+        map<int,int> m,m1;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
         }
-        for (int i: A) {
-            if (left[i] == 0) continue;
-            left[i]--;
-            if (end[i - 1] > 0) {   
-                end[i - 1]--;
-                end[i]++;
-            } else if (left[i + 1] > 0 && left[i + 2] > 0) {
-                left[i + 1]--;
-                left[i + 2]--;
-                end[i + 2]++;
-            } else {   
-                return false;
+        for(auto i :nums){
+            if(m[i]==0)
+                continue;
+            m[i]--;
+            if(m1[i-1]>0){
+                m1[i-1]--;
+                m1[i]++;
             }
+            else if(m[i+1]!=0 && m[i+2]!=0){
+                m[i+1]--;
+                m[i+2]--;
+                m1[i+2]++;
+            }
+            else
+                return false;
         }
         return true;
     }
