f309f0a2a79490726e32c4852ee02643fa509507
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-08-22 22:47:00 +0530

    Time: 3 ms (58.67%), Space: 5.8 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
    bool isPowerOfFour(const int& num) {
-	return num > 0 && (num & (num - 1)) == 0 && (num & 0xAAAAAAAA) == 0;
+	return num > 0 && (num & (num - 1)) == 0 && (num %3==1);
 }
 };
