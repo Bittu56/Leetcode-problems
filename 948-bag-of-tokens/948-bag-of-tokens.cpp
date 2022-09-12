ca7266a1b899314c51e51d8470ccf45a995a1e82
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2022-09-12 23:49:48 +0530

    Time: 9 ms (61.88%), Space: 10.6 MB (66.86%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-       sort(tokens.begin(),tokens.end());
+        sort(tokens.begin(),tokens.end());
         
-        int i=0;int j = tokens.size();int n=tokens.size();
+        int i, j,n;
+        i=0;
+        j=tokens.size();
+        n=tokens.size();
         while(i<j)
         {
             if(power>=tokens[i])
                 power-=tokens[i++];
-            else if(i-( n-j ) && j > i+1)
+            else if(i-(n-j) && j > i+1)    ///score or not 
             {
                 power+=tokens[--j];
-                
             }
-            else
-                break;
+            else 
+               break;
         }
         
-    return i-(n-j);
+        return i-(n-j);
+        
     }
 };
