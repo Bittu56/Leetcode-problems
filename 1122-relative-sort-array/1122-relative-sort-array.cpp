58832e499504b41e19baefcd31bf9b28a83dba43
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2024-06-11 20:26:00 +0530

    Time: 6 ms (17.85%), Space: 10.1 MB (54.14%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-	vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-		map<int, int> m;
-		for(int i = 0; i < arr1.size(); i++){
-			m[arr1[i]]++;
-		}
-		vector<int> res;
-
-		for(int i = 0; i < arr2.size(); i++){
-			for(int j = 0; j < m[arr2[i]]; j++){
-				res.push_back(arr2[i]);
-			}
-			m.erase(arr2[i]);
-		}
-
-		for(auto i : m){
-			for(int j = 0; j < m[i.first]; j++){
-				res.push_back(i.first);
-			}
-		}
-		return res;
-	}
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> ans;
+        map<int, int>mp;
+        
+        for(auto x:arr1)
+        {
+            mp[x]++;
+        }
+        
+    for(auto temp: arr2)
+    {
+        if(mp.find(temp)!=mp.end())
+        {
+            auto x= mp.find(temp);
+            int count= x->second;
+            vector<int> res(count,temp);
+            ans.insert(ans.end(),res.begin(), res.end());
+            mp.erase(temp);
+        }
+    }
+        
+        for(auto x:mp)
+        {
+            int y= x.first;
+            int m=x.second;
+            vector<int>v(m,y);
+            ans.insert(ans.end(), v.begin(), v.end());
+            
+        }
+        
+        return ans;
+        
+    }
 };
