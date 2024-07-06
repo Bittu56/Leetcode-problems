5974947cababf26ce73a918ae4c6253b00b709b5
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2024-07-06 18:54:09 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (30.58%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
         
-        int idx=1;
-        int dir=1;
-        while(time)
+        int timeleft=time% (n-1);
+        int round= time/(n-1);
+        if(round%2==0)
         {
-            if(idx+dir>=1 && idx+dir<=n)
-            {
-            idx+=dir;
-            time--;
-            }
-            else
-            {
-                dir*=-1;
-            }
+            return 1+timeleft;
+            
         }
+        else
+            return n-timeleft;
         
         
-        return idx;
+        return -1;
     }
 };
