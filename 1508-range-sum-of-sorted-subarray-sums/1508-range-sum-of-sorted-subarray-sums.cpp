76481ae125f80e2e469c688d9c042bcdabcf73f4
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2024-08-05 09:03:10 +0530

    Time: 87 ms (70.99%), Space: 27.3 MB (31.38%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-int rangeSum(vector<int>& nums, int n, int left, int right) {
-	vector<int> sums { };
-	for (int i = 0; i < n; i++) {
-		int prefixSum = 0;
-		for (int j = i; j < n; j++) {
-			prefixSum += nums[j];
-			sums.push_back(prefixSum);
-		}
-	}
-	sort(begin(sums), end(sums));
-	int res = accumulate(sums.begin() + left - 1, sums.begin() + right, 0LL) % 1000000007;
-	return res;
-}
+    int M =1e9+7;
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        vector<int>temp;
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            
+            for(int j=i;j<n;j++)
+            {
+                sum+=nums[j];
+                temp.push_back(sum);
+            }
+        }
+        
+        sort(temp.begin(),temp.end());
+  int ans=0;
+        for(int i=left-1;i<=right-1;i++)
+        {
+            ans=(ans+temp[i])%M;
+        }
+        
+        
+        return ans;
+        
+    }
 };
