7951fee067022cf5d8638626603747fefcdc007c
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2024-09-12 20:29:12 +0530

    Time: 20 ms (39.79%), Space: 18.5 MB (41.63%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,40 +11,36 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int k = lists.size(); // taking size of the list
-        if(k == 0) // if size is zero
-            return NULL; // simply return NULL
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>,
+        greater<pair<int, ListNode*>>> pq;
         
-        // making a vector pair where first contain value and second contain node
-        vector<pair<int, ListNode*>> arr; 
-        
-        for(int i = 0; i < k; i++) // traverse all over the list
+        for(int i=0;i<lists.size();i++)
         {
-            ListNode* curr_list = lists[i]; // extracting current linked list
-            
-            while(curr_list != NULL) // while current linked list is NOT NULL
+            if(lists[i])
             {
-                arr.push_back({curr_list -> val, curr_list}); // push into vector
-                curr_list = curr_list -> next;
+                pq.push({lists[i]->val,lists[i]});
             }
         }
-        
-        // this does not gurantee that k is zero, 
-        // suppose an array like this [[],[],[],],here k = 3 and size of array is 0
-        if(arr.size() == 0) // if their is no element i.e zero element
-            return NULL;
-        
-        sort(arr.begin(), arr.end()); // sort the vector on the basis of values
-        
-        // start making links b/w the elements of vector
-        for(int i = 0; i < arr.size() - 1; i++)
+        ListNode * dump = new ListNode(-1);
+        ListNode* temp=dump;
+        while(!pq.empty())
         {
-            arr[i].second -> next = arr[i + 1].second;
+            
+            auto it=pq.top();
+            pq.pop();
+            
+            if(it.second->next)
+            {
+                pq.push({it.second->next->val,it.second->next});
+            }
+            
+            temp->next=it.second;
+            temp=temp->next;
+            
+            
         }
         
-        // in the next of last node put NULL
-        arr[arr.size() - 1].second -> next = NULL;
+        return dump->next;
         
-        return arr[0].second; // return first node of the vector
     }
 };
