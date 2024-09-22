95c293dfbb21f23b26132b285659128ac0e72640
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2024-09-22 23:35:19 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (56.67%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int n= matrix[0].size(), m= matrix.size();
-        
-        int top=0,down=m-1,left=0,right=n-1, count=0;
-        
-        while(top<=down && left<=right){
-            if(count==0){
-                for(int i=left; i<=right; i++)
-                    ans.push_back(matrix[top][i]);
-                top++; count++;
-            }
-            else if(count==1){
-                for(int i=top; i<=down; i++)
-                    ans.push_back(matrix[i][right]);
-                right--; count++;
-            }
-            else if(count==2){
-                for(int i=right; i>=left; i--)
-                    ans.push_back(matrix[down][i]);
-                down--; count++;
-            }
-            else if(count==3){
-                for(int i=down; i>=top; i--)
-                    ans.push_back(matrix[i][left]);
-                left++; count=0;
-            }
-        }
-        
-        return ans;
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+          vector<int> ans;
+ 
+  int n = mat.size(); // no. of nows
+  int m = mat[0].size(); // no. of columns
+  
+  // Initialize the pointers reqd for traversal.
+  int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+  // Loop until all elements are not traversed.
+  while (top <= bottom && left <= right) {
+      
+    // For moving left to right
+    for (int i = left; i <= right; i++)
+      ans.push_back(mat[top][i]);
+
+    top++;
+
+    // For moving top to bottom.
+    for (int i = top; i <= bottom; i++)
+      ans.push_back(mat[i][right]);
+
+    right--;
+    
+    // For moving right to left.
+    if (top <= bottom) {
+      for (int i = right; i >= left; i--)
+       ans.push_back(mat[bottom][i]);
+
+      bottom--;
     }
+
+    // For moving bottom to top.
+    if (left <= right) {
+      for (int i = bottom; i >= top; i--)
+        ans.push_back(mat[i][left]);
+
+      left++;
+    }
+  }
+  return ans;
+}
+
+    
 };
