2ad58c0cb48ffaedddd03537c03e5d42326d8ba2
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2024-10-19 12:27:20 +0530

    Time: 821 ms (24.57%), Space: 39.5 MB (60.77%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-
+    
     int result;
     int K;
-    void dfs(vector<int> &nums, int idx, unordered_map<int, int> &mp) {
-
-        if(idx == nums.size()) {
+    void dfs(vector<int>&nums, int idx, unordered_map<int,int>&mp)
+    {
+        if(idx==nums.size())
+        {
             result++;
-            return;
+            return ;
         }
-
-        //not_take
-        dfs(nums, idx+1, mp);
+         dfs(nums, idx+1, mp);
+        if(!mp[nums[idx]-K] && !mp[nums[idx]+K])
+        {
+            mp[nums[idx]]++;
+            dfs(nums,idx+1, mp);
+            mp[nums[idx]]--;
+            
+        }
         
-        //checking if we can take it or not
-        if(!mp[nums[idx] - K] && !mp[nums[idx] + K]) {
-                mp[nums[idx]]++;
-                dfs(nums, idx + 1, mp);
-                mp[nums[idx]]--;
-        }
+        
     }
-
+    
     int beautifulSubsets(vector<int>& nums, int k) {
-
-        result = 0;
-        K = k;
-
-        unordered_map<int, int> mp;
- 
-        dfs(nums, 0, mp);
-
-        return result - 1; //-1 because we don't want to count empty subset in result
+        result=0;
+        K=k;
+        unordered_map<int,int>mp;
+        dfs(nums,0,mp);
+        return result-1;
+        
     }
 };
