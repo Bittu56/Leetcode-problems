86a6df7c6e8466951e5c2923a02c7c53861e4200
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2024-11-08 13:18:25 +0530

    Time: 3 ms (67.62%), Space: 95.2 MB (92.93%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size();
+
+        vector<int> result(n);
+
+        //step-1 : Find the total xor
+        int XOR = 0;
+        for(int i = 0; i < n; i++) {
+            XOR ^= nums[i];
+        }
+
+        //To find flip, first find the mask having all bits set to 1
+        int mask = ((1 << maximumBit) - 1);
+
+        for(int i = 0; i < n; i++) {
+            int k = XOR ^ mask; //this will give me the flipped value of XOR i.e. my best K
+            result[i] = k;
+
+            XOR = (XOR ^ nums[n-1-i]);
+        }
+
+        return result;
+
+
+    }
+};
