ca6e175cf42becb15eeb42657d64e13b5f5f621f
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2025-08-24 21:30:03 +0530

    Time: 0 ms (100%), Space: 45.2 MB (27.17%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int n = nums.size();
+
+        int currSubSum = nums[0];
+        int maxSubSum = nums[0];
+
+        //Kadane's Algo for findig max subarray sum
+        for(int i = 1; i < n; i++) {
+            currSubSum = max(nums[i], currSubSum + nums[i]);
+            maxSubSum = max(maxSubSum, currSubSum);
+        }
+
+        //Kadane's Algo for findig min subarray sum
+        int minSubSum = nums[0];
+        currSubSum = nums[0];
+        for(int i = 1; i < n; i++) {
+            currSubSum = min(nums[i], currSubSum + nums[i]);
+            minSubSum = min(minSubSum, currSubSum);
+        }
+
+        return max(maxSubSum, abs(minSubSum));
+    }
+};
