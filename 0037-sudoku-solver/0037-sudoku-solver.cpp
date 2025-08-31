da52797faba087234ed652e289aed69c7162d743
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2025-08-31 16:39:43 +0530

    Time: 11 ms (98.12%), Space: 8.8 MB (62.78%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,44 +1,63 @@
 class Solution {
+	unsigned Row[9]={0};
+    unsigned Col[9]={0};
+    unsigned Block[9]={0};
+    vector<pair<char, char>> uncertain;
 public:
-    bool solve(vector<vector<char>>& board){    
-        for(int i=0; i<board.size(); i++){
-            for(int j=0; j<board[0].size(); j++){
+     void print(vector<vector<char>>& board){
+        for(vector<char>& row: board){
+            for(char& c: row)
+                cout<<c;
+            cout<<endl;
+        }
+        cout<<"===========\n";
+    }
+    void set3Cond(int i, int j,  int x){
+        const int x2=1<<x;
+		Row[i]|=x2;
+        Col[j]|=x2;
+        const int bidx=(i/3)*3 +j/3;
+        Block[bidx]|=x2;
+	}
 
-                if(board[i][j] == '.'){
-                    for(char c='1'; c<='9'; c++){
-                        if(isValid(board, i, j, c)){
-                            board[i][j] = c;
-
-                            if(solve(board) == true){
-                                return true;
-                            }
-                            else
-                                board[i][j] = '.';
-                        }
-                    }
-
-                    return false;
+    void setup(vector<vector<char>>& board) {
+        uncertain.reserve(81);
+        for (int i=0; i<9; i++) {
+            for (int j=0; j< 9; j++) {
+                char c=board[i][j];
+                if ( c== '.') {
+                    uncertain.emplace_back(i, j);
+                }
+                else {
+                    set3Cond(i, j, c-'1');
                 }
             }
         }
-
-        return true;   // for fully filled board
     }
 
-    bool isValid(vector<vector<char>>& board, int row, int col, char c){
-        for(int i=0; i<9; i++){
-            if(board[i][col] == c)   // checks within column
-                return false;
-            if(board[row][i] == c)   // checks within row
-                return false;
-            if(board[3 * (row/3)  +  i/3][3 * (col/3)  +  i%3]   ==   c)  // checks within submatrix
-                return false;
-        }
+    bool solve(vector<vector<char>>& board, int idx) {
+        if (idx==uncertain.size()) return 1; 
+        auto [i, j]=uncertain[idx];
+        const int  bidx=(i/3)*3 +j/3;
+        unsigned notMask=~(Row[i]|Col[j]|Block[bidx]) & 0b111111111;
+        unsigned Bit=0;
+        for (; notMask; notMask^=Bit) {
+            Bit=bit_floor(notMask);
+            const int x=countr_zero(Bit);
 
-        return true;
+            board[i][j]='1'+x;
+            Row[i]|=Bit; Col[j]|=Bit; Block[bidx]|=Bit;
+            if (solve(board, idx+1)) return 1;
+            Row[i]^=Bit; Col[j]^=Bit; Block[bidx]^=Bit;// backtracking
+            board[i][j]='.';
+        }
+        return 0; 
     }
 
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+    //    print(board);
+        setup(board);
+        solve(board, 0);
+    //    print(board);
     }
 };
