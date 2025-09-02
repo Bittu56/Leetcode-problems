a329422de2ff7c8554b5658ef1dab0ca9c83ad30
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2025-09-02 20:49:23 +0530

    Time: 0 ms (100%), Space: 8.5 MB (7.27%) - LeetHub

---
+++ 1252-break-a-palindrome/1252-break-a-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string breakPalindrome(string pal) {
+        int n = pal.length();
+        if(n == 1) return "";
+        
+        for(int i = 0; i<n/2; i++) {
+            if(pal[i] != 'a') {
+                pal[i] = 'a';
+                return pal;
+            }
+        }
+        
+        pal[n-1] = 'b';
+        return pal;
+    }
+};
