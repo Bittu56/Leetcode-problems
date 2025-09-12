b550695c26330c23b86b4c29bb3f4e43a005c52f
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2025-09-12 06:21:29 +0530

    Time: 0 ms (100%), Space: 23.4 MB (16.41%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        
+        vector<int> L2R(n, 1);
+        vector<int> R2L(n, 1);
+        
+        //First comparing with only left neighbour
+        for(int i = 1; i<n; i++) {
+            if(ratings[i] > ratings[i-1])
+                L2R[i] = max(L2R[i], L2R[i-1]+1);
+        }
+        
+        //Then comparing with only right neighbour
+        for(int i = n-2; i>=0; i--) {
+            if(ratings[i] > ratings[i+1])
+                R2L[i] = max(R2L[i], R2L[i+1]+1);
+        }
+        
+        
+        int result = 0;
+        for(int i = 0; i<n; i++) {
+            result += max(L2R[i], R2L[i]);
+        }
+        
+        return result;
+    }
+};
