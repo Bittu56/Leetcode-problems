9bbf44ad81b61c1c02d530082a5cc4d80574d762
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2025-09-15 22:11:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (87.73%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.length();
+
+        vector<int> result;
+
+        //last occurance of each character
+        //unordered_map<char, int> mp;
+        vector<int> mp(26, -1);
+        for(int i = 0; i < n; i++) {
+            int idx = s[i] - 'a';
+            mp[idx] = i;
+        }
+
+        int i = 0;
+        while(i < n) {
+            int end = mp[s[i]-'a'];
+
+            int j = i;
+            while(j < end) {
+                end = max(end, mp[s[j]-'a']);
+                j++;
+            }
+            result.push_back(j-i+1);
+            i = j+1;
+        }
+
+        return result;
+    }
+};
