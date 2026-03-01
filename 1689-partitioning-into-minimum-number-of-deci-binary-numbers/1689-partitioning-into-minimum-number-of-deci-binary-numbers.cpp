f0c375d6932b445ed7aa41f2fd3033f831aca86a
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2026-03-01 13:22:09 +0530

    Time: 27 ms (5.68%), Space: 16.5 MB (53.96%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
-    int minPartitions(string& n) {
-	int ans = 0;
-	for(auto& ch : n) ans = max(ans, ch - '0');
-	return ans;
-        
+    int minPartitions(string n) {
+        int count = 0;
+
+        while (true) {
+            bool changed = false;
+
+            for (char &ch : n) {
+                if (ch != '0') {
+                    ch--;          // subtract 1
+                    changed = true;
+                }
+            }
+
+            if (!changed)
+                break;
+
+            count++;
+        }
+
+        return count;
     }
 };
