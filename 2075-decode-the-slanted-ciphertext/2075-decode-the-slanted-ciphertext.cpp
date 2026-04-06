572d17f3fdd4266cb67ca8ae03d0073eb5c7c461
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2026-04-06 09:40:57 +0530

    Time: 59 ms (5.66%), Space: 46.2 MB (26.13%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int l       = encodedText.size();
+        int columns = l / rows;
+
+        string originalText;
+        for (int col = 0; col < columns; col++) {
+            for (int j = col; j < l; j += (columns + 1)) {
+                originalText += encodedText[j];
+            }
+        }
+
+        //Remove spaces put at the end
+        while (!originalText.empty() && originalText.back() == ' ') {
+            originalText.pop_back();
+        }
+        
+        return originalText;
+    }
+};
