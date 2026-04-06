6483c1c3416fa3e4148c8fd1490eff39b37f00ac
Bitsfrost <38579966+Bittu56@users.noreply.github.com>
2026-04-06 09:26:52 +0530

    Time: 0 ms (100%), Space: 10.1 MB (95.36%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+
+        for(char &ch : moves) {
+            if(ch == 'U')       y++;
+            else if(ch == 'D')  y--;
+            else if(ch == 'L')  x--;
+            else if(ch == 'R')  x++;
+        }
+
+        return x == 0 && y == 0;
+    }
+};
